aa5b8d988b5df6bd798db12f39e3af61e9f1d2d6
Wim Van Laer <van.laer.wim@gmail.com>
2022-06-28 17:53:11 +0200

    feat: made EventHubImpl exported

--- lib/hawk.dart
+++ lib/hawk.dart
@@ -1,6 +1,7 @@
 library hawk;
 
 export 'package:hawk/src/event_hub.dart';
+export 'package:hawk/src/event_hub_impl.dart';
 export 'package:hawk/src/handler.dart';
 export 'package:hawk/src/handler_store/handler_store.dart';
 export 'package:hawk/src/handler_store/handler_store_impl.dart';
