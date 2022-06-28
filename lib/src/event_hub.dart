b193a51709ec2d00e958486a10fb52a331139154
Wim Van Laer <van.laer.wim@gmail.com>
2022-06-28 17:49:08 +0200

    feat: added default instance

--- lib/src/event_hub.dart
+++ lib/src/event_hub.dart
@@ -1,6 +1,11 @@
 import 'package:hawk/hawk.dart';
+import 'package:hawk/src/event_hub_impl.dart';
+
+EventHub? _instance;
 
 abstract class EventHub {
+  factory EventHub.instance() => _instance ?? (_instance = EventHubImpl());
+
   HandlerStore get handlers;
 
   void send<T>(T event);
